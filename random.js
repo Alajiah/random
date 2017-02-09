31d655b7d737c71c50911ff3d4430f7a051846d8
Alajiah <tropical.lay14@gmail.com>
2017-02-09 13:42:10 -0800

    pizza

--- random.js
+++ random.js
@@ -5,7 +5,7 @@ var namespace = "http://www.w3.org/2000/svg"
 // You need to use at least 3 different types of shape.
 // Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
 function createFirstScene() {
-    
+makeCircle("150, 50, 50", "purple",0.15)    
 }
 
 
@@ -13,7 +13,7 @@ function createFirstScene() {
 // You need to use at least 3 different types of shape.
 // Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
 function createSecondScene() {
-    
+ makeText("Cake has many layers ", 50, 50, 10, "sans-serif", "navy" 0.4)   
 }
 
 
@@ -21,7 +21,7 @@ function createSecondScene() {
 // You need to use at least 3 different types of shape.
 // Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
 function createThirdScene() {
-    
+ makeCircle("150, 50, 50", "cyan", 0.15)   
 }
 
 
@@ -32,14 +32,18 @@ function createThirdScene() {
 // This function is called whenever you press the "Go!" button.
 function createRandomScene() {
     // Generate a random number between 0 and 1, and store it in a variable.
-    
+     var myNumber = Math.random()
+     alert(myNumber)
     // If the number is less than 0.33, call the function to create your first scene.
-    
-    
+  var myNumber = Math.random()
+  if(myNumber < 0.33){
+      createFirstScene()
+  }
     
     // Else, if the number is less than 0.67, call the function to create your second scene.
     
-    
+    var myNumer = Math.random()
+    if
     
     // Else, call the function to create your third scene.
     
